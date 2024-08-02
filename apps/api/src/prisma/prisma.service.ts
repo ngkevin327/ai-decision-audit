97c6c56dda7d646bf21aaf4c0aa87b04c2757292
Kevin Ng <ngkevin3327@gmail.com>
2024-08-02 14:53:49 -0400

    feat(api): integrate Prisma and database connection module

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { AppConfigService } from '../config/config.service';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(config: AppConfigService) {
+    super({
+      datasources: {
+        db: { url: config.databaseUrl },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
